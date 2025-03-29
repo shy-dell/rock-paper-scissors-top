f8e47fe61bfaaa185a1de950243e5ad68dad559b
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-29 00:19:14 -0400

    create playGame, add round count, assign values for compare

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 function getComputerChoice() {
     let compChoice
-    const randNum = Math.floor(Math.random() * 100)
+    let randNum = Math.floor(Math.random() * 100)
     if (randNum <= 33) {
         compChoice = "Rock";
+        randNum = 1;
     } else if (randNum <= 66) {
         compChoice = "Paper";
+        randNum = 2;
     } else {
         compChoice = "Scissors";
+        randNum = 3;
     }
-    console.log(compChoice);
+    return compChoice, randNum;
 }
 
 // getComputerChoice();
@@ -17,14 +20,37 @@ const playButton = document.querySelector("#playButton");
 
 function getHumanChoice() {
     let humanChoice = prompt("Input one of: 'Rock', 'Paper', 'Scissors' to play.", "Rock");
-    console.log(humanChoice);
+    let humanChoiceVal;
+    if (humanChoice.toLowerCase() == "rock") {
+        humanChoiceVal = 1;
+    } else if (humanChoice.toLowerCase() == "paper"){
+        humanChoiceVal = 2;
+    } else {
+        humanChoiceVal = 3;
+    }
+    return humanChoiceVal;
 }
 // getHumanChoice()
-function playRound(human,computer) {
-    console.log(human);
-    console.log(computer);
+function playRound(hChoice,cChoice) {
+    if (hChoice.toLowerCase() === cChoice.toLowerCase()) {
+        console.log("It's a tie!");
+
+    }
 
 }
-const hSelection = getHumanChoice();
-const cSelection = getComputerChoice();
-playRound(hSelection,cSelection);
+
+function playGame() {
+    let roundCount = 1;
+    let hScore = 0;
+    let cScore = 0;
+
+    while (roundCount < 6) {
+        console.log(`This is game ${roundCount}`)
+        
+        const hSelection = getHumanChoice();
+        const cSelection = getComputerChoice();
+        playRound(hSelection,cSelection);
+        
+        roundCount ++ ;
+    };
+};
