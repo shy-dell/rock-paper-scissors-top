40681a6a819c3094a0648f9e362e06af0840db25
shy-dell <76672306+shy-dell@users.noreply.github.com>
2025-03-28 23:37:53 -0400

    Add function to randomize computer choice

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+    let compChoice
+    const randNum = Math.floor(Math.random() * 100)
+    if (randNum <= 33) {
+        compChoice = "Rock";
+    } else if (randNum <= 66) {
+        compChoice = "Paper";
+    } else {
+        compChoice = "Scissors";
+    }
+    console.log(compChoice);
+}
+
+getComputerChoice();
+
+function getHumanChoice() {
+    let humanChoice = prompt("Input one of: 'Rock', 'Paper', 'Scissors' to play.", "Rock");
+    console.log(humanChoice);
+}
